098f80cd558f0e663c6227487d271954b0160d44
Jean Chquimarca <jeanchuquimarca12@gmail.com>
2018-05-31 10:17:13 -0500

    ejercicio de arreglos

---
+++ Clase9/src/clase9/Clase9.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase9;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Clase9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] c;
+        c = new int[12];
+
+        System.out.printf("%s\t\t%s\n", "Indice", "Valor");
+        for (int contador = 0; contador < c.length; contador++) {
+            System.out.printf("%d\t\t%d\n", contador, c[contador]);
+        }
+
+    }
+
+}
